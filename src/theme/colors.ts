67037713aab3dca5776371ca8d9654c89780d00c
Phillipi Alves <phixelz@gmail.com>
2022-08-04 09:34:00 -0300

    theme update

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,17 +1,23 @@
 export default {
-    colors: {
-      primary: '#663CC0',
-      primary_light: '#FCFCFC',
-      primary_dark: '#603CAC',
-  
-      secondary: '#F0F0F0',
-      secondary_light: '#C9C9C9',
-      secondary_dark: '#373737',
+  colors: {
+    primary: '#663CC0',
+    primary_light: '#FCFCFC',
+    primary_dark: '#603CAC',
 
-      attention: '#E3D65A',
-      success: '#4FDA8F',
-  
-      white: '#FFFFFF',
-    },
-  };
-  
+    secondary: '#F0F0F0',
+    secondary_light: '#E7E7E7',
+    secondary_dark: '#373737',
+
+    title: '#404040',
+    subtitle: '#AAAAAA',
+
+    attention: '#E3D65A',
+    success: '#4FDA8F',
+    warning: '#B95249',
+
+    white: '#FFFFFF',
+
+    gray: '#C9C9C9',
+    purple: '#F6F2FF',
+  },
+};
